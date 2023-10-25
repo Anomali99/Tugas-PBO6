a9ab6e1992c842796111fb37f9fe0cd6e9c5d165
Anomali99 <fatiqnur@gmail.com>
2023-10-25 20:21:35 +0700

    first commit

---
+++ src/latihan/Santri.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan;
+
+/**
+ *
+ * @author fatiq
+ */
+public class Santri {
+    private String id;
+    private String nama;
+    private String jk;
+    private String alamat;
+    private String tlp;
+    private int usia;
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJk() {
+        return jk;
+    }
+
+    public void setJk(String jk) {
+        this.jk = jk;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTlp() {
+        return tlp;
+    }
+
+    public void setTlp(String tlp) {
+        this.tlp = tlp;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+
+    public void setUsia(int usia) {
+        this.usia = usia;
+    }
+    
+}
